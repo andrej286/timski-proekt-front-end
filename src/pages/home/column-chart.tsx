ea4e4c660f539d70e64df5ea02a4047e840175d4
andrej286 <mikistojanovski6@gmial.com>
2023-09-25 18:52:30 +0200

    Chart date filtering added

--- src/pages/home/column-chart.tsx
+++ src/pages/home/column-chart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo, useState} from 'react';
 import ReactApexChart from 'react-apexcharts';
 import {ApexOptions} from "apexcharts";
 import {Purchase} from "./home";
@@ -7,8 +7,19 @@ const monthNames = [
     "January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"
 ];
+const previousYears = [
+    "2023", "2022", "2021", "2020", "2019", "2018", "2017",
+    "2016", "2015", "2014", "2013", "2012"
+];
 
 export const ColumnChart = (purchases: any) => {
+
+    const [selectedYear, setSelectedYear] = useState('2023');
+
+    const filteredPurchases = useMemo(
+        () => purchases.purchases?.filter(
+            (purchase: Purchase) => { return new Date(purchase.date).getFullYear().toString() == selectedYear}),
+        [purchases, selectedYear]);
 
     const chartOptions: ApexOptions = {
         chart: {
@@ -22,16 +33,26 @@ export const ColumnChart = (purchases: any) => {
     const chartSeries = [
         {
             name: 'Series 1',
-            data: purchases.purchases?.map((item: Purchase) => item?.amount),
+            data: filteredPurchases.map((purchase: Purchase) => purchase?.amount),
         },
     ];
 
     return (
-        <ReactApexChart
-            options={chartOptions}
-            series={chartSeries}
-            type="bar"
-            height={350}
-        />
+        <>
+            <select value={selectedYear} onChange={(e) => {setSelectedYear(e.target.value)}}>
+                <option value="">Select</option>
+                {previousYears.map((year) => (
+                    <option key={year} value={year}>
+                        {year}
+                    </option>
+                ))}
+            </select>
+            <ReactApexChart
+                options={chartOptions}
+                series={chartSeries}
+                type="bar"
+                height={350}
+            />
+        </>
     );
 };
